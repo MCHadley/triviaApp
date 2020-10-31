ba2f5abc574593c44069e426472a285865cc62b3
Michael <michael.c.hadley@gmail.com>
2020-10-30 22:43:01 -0400

    Create API to send JSON file to JS

--- api/questionserver.py
+++ api/questionserver.py
@@ -1,7 +1,15 @@
 from flask import Flask, request, Response, jsonify
+import json
 
 app = Flask(__name__)
+
+with open('Apprentice_TandemFor400_Data.json', "r") as file:
+    questions = json.load(file)
 
 @app.route('/')
 def index():
     return 'you are connected'
+
+@app.route('/questions')
+def questionsServer():
+    return jsonify(questions)
