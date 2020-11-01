b0037784577dfe096f2e942a65f82f36ae0c3f28
Michael <michael.c.hadley@gmail.com>
2020-11-01 18:01:23 -0500

    Style and score counter created

--- js/trivia.js
+++ js/trivia.js
@@ -10,11 +10,22 @@ function getData(){
 
 function questionBuilder(data){
     for(i=0;i<data.length;i++){
-        $('#trivia').append('<form class='+ 'card'+ i + '><text>'+ data[i].question +'</text></form>');
-        $('.card' + i).append('<br><input type="radio" id='+ data[i].correct +' value="correct" name="question' + i + '">' + '<label for="' + data[i].correct + '">'+ data[i].correct +'</label>');
+        $('#trivia').append('<form class="cardModal" id='+ 'card'+ i + '><text>'+ data[i].question +'</text></form>');
+        $('#card' + i).append('<br>'+ '<label for="' + data[i].correct + '">' +'<input type="radio" id=question'+ i +' value="correct" name="question' + i + '">' + data[i].correct +'</label>');
         var incorrect = data[i].incorrect;
         for(c=0;c<incorrect.length;c++){
-            $('.card' + i).append('<br><input type="radio" id='+ incorrect[c] +' value="incorrect" name="question'+ i + '"><label for="' + incorrect[c] + '">'+ incorrect[c] +'</label>')
+            $('#card' + i).append('<br><label for="' + incorrect[c] + '"><input type="radio" id=question'+ c +' value="incorrect" name="question'+ i + '">' + incorrect[c] + '</label>');
         }
+        $('#card' + i).css('card')
+        $('#card' + i).append('<br><button type="button" value="next">Next</button>')
     }
+    $('#trivia').on('click', e => {
+        let div = $("#trivia");
+        let rbs = div.find('input[type="radio"][value="correct"]');
+        let rbsChecked = 0;
+        $(rbs).each((i, e) => {
+          if (e.checked)
+            rbsChecked++;
+        });
+      });
 }
